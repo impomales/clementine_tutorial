e5728a24925ee9dda0f4c6f9ed975e9f789e749e
Isaias <impomales@gmail.com>
2016-09-18 19:39:10 +0000

    connects to database

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = function(app) {
+module.exports = function(app, db) {
     app.route('/')
         .get(function(req, res) {
             res.sendFile(process.cwd() + '/public/index.html')
--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 
 var express = require('express'),
     routes = require('./app/routes/index.js'),
+    mongo = require('mongodb').MongoClient,
     app = express()
     
-app.use('/public', express.static(process.cwd() + '/public'))
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
+mongo.connect('mongodb://' + process.env.IP + ':27017/clementinedb', function(err, db) {
+    if (err) throw new Error('Database failed to connect!')
+    else console.log('MongoDB successfully connected on port 27017.')
     
-routes(app)
-
-var server = app.listen(process.env.PORT || 8080, function() {
-    console.log('Listending on port ' + server.address().port)
+    app.use('/public', express.static(process.cwd() + '/public'))
+    app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
+        
+    routes(app, db)
+    
+    var server = app.listen(process.env.PORT || 8080, function() {
+        console.log('Listening on port ' + server.address().port)
+    })
 })
