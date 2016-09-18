18ef79cab29d682fc96236d3597be69d86f17322
Isaias <impomales@gmail.com>
2016-09-18 19:34:07 +0000

    homepage outline, and reference to static files

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var express = require('express'),
     routes = require('./app/routes/index.js'),
     app = express()
     
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
+    
 routes(app)
 
 var server = app.listen(process.env.PORT || 8080, function() {
