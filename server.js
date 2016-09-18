767a8acab59c8bcefa3ff91e9ff76eba46c7ed18
Isaias <impomales@gmail.com>
2016-09-18 19:24:31 +0000

    refactored get request in routes file

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 'use strict'
 
-var express = require('express')
-var app = express()
-
-app.get('/', function(req, res) {
-    res.sendFile(process.cwd() + '/index.html')
-})
+var express = require('express'),
+    routes = require('./app/routes/index.js'),
+    app = express()
+    
+routes(app)
 
 var server = app.listen(process.env.PORT || 8080, function() {
     console.log('Listending on port ' + server.address().port)
