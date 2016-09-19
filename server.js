20b3881beb5789d7e9d0137904278abd7e7eae5b
Isaias <impomales@gmail.com>
2016-09-19 20:02:01 +0000

    installed mongoose, passport, and added a database schema for clicks

--- server.js
+++ server.js
@@ -2,19 +2,16 @@
 
 var express = require('express'),
     routes = require('./app/routes/index.js'),
-    mongo = require('mongodb').MongoClient,
+    mongoose = require('mongoose'),
     app = express()
     
-mongo.connect('mongodb://' + process.env.IP + ':27017/clementinedb', function(err, db) {
-    if (err) throw new Error('Database failed to connect!')
-    else console.log('MongoDB successfully connected on port 27017.')
+mongoose.connect('mongodb://' + process.env.IP + ':27017/clementinedb')
     
-    app.use('/public', express.static(process.cwd() + '/public'))
-    app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
-        
-    routes(app, db)
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
     
-    var server = app.listen(process.env.PORT || 8080, function() {
-        console.log('Listening on port ' + server.address().port)
-    })
+routes(app)
+
+var server = app.listen(process.env.PORT || 8080, function() {
+    console.log('Listening on port ' + server.address().port)
 })
