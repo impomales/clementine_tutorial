6df86ff90d480be7a4963655e510ee9ea86f282c
Isaias <impomales@gmail.com>
2016-09-19 20:40:41 +0000

    mongoose set up complete

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -1,35 +1,33 @@
 'use strict';
 
-function clickHandler(db) {
-    var clicks = db.collection('clicks')
-    
+var Clicks = require('../models/clicks.js')
+
+function ClickHandler() {
     this.getClicks = function(req, res) {
         var clickProjection = {'_id': false}
         
-        clicks.findOne({}, clickProjection, function(err, result) {
-            if (err) throw new Error('Query error in collection "clicks".')
+        Clicks
+            .findOne({}, clickProjection)
+            .exec(function(err, result) {
+                if (err) throw new Error('Query error in collection "clicks".')
             
-            if (result) res.json(result)
-            else {
-                clicks.insert({'clicks': 0}, function(err) {
-                    if (err) throw new Error('Error in insertion of initial click.')
-                    
-                    clicks.findOne({}, clickProjection, function(err, doc) {
-                        if (err) throw new Error('Query error after inserting initial click')
-                        
-                        res.json(doc)
+                if (result) res.json(result)
+                else {
+                    var newDoc = new Clicks({'clicks': 0})
+                    newDoc.save(function(err, doc) {
+                       if (err) throw new Error('Error in insertion of initial click.')
+                       
+                       res.json(doc)
                     })
-                })
+                }
             }
-        })
+        )
     }
     
     this.addClick = function(req, res) {
-        clicks.findAndModify(
-            {},
-            {'_id': 1},
-            { $inc: {'clicks': 1} },
-            function(err, result) {
+        Clicks
+            .findOneAndUpdate({}, { $inc: {'clicks': 1} })
+            .exec(function(err, result) {
                 if (err) throw new Error('Error updating click count to database.')
                 
                 res.json(result)
@@ -38,11 +36,15 @@ function clickHandler(db) {
     }
     
     this.resetClicks = function(req, res) {
-        clicks.update({}, {'clicks': 0}, function(err, result) {
-            if (err) throw new Error('Error in resetting click count')
-            res.json(result)
-        })
+        Clicks
+            .findOneAndUpdate({}, {'clicks': 0})
+            .exec(function(err, result) {
+                if (err) throw new Error('Error in resetting click count')
+                
+                res.json(result)
+            }
+        )
     }
 }
 
-module.exports = clickHandler
+module.exports = ClickHandler
