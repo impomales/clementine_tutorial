f7f8745c676e3ccc73199f65918d2acc6758c757
Isaias <impomales@gmail.com>
2016-09-23 04:12:28 +0000

    server side passport integration complete

--- app/config/passport.js
+++ app/config/passport.js
@@ -19,26 +19,26 @@ module.exports = function(passport) {
         clientID: configAuth.githubAuth.clientID,
         clientSecret: configAuth.githubAuth.clientSecret,
         callbackURL: configAuth.githubAuth.callbackURL
-    }), function(token, refreshToken, profile, done) {
-        process.nextTick(function() {
-            User.findOne({'github.id': profile.id}, function(err, user) {
-                if (err) return done(err)
-                if (user) return done(null, user)
-                else {
-                    var newUser = new User()
-                    
-                    newUser.github.id = profile.id
-                    newUser.github.username = profile.username
-                    newUser.github.displayName = profile.displayName
-                    newUser.github.publicRepos = profile._json.public_repos
-                    newUser.nbrClicks.clicks = 0
-                    
-                    newUser.save(function(err) {
-                        if (err) throw new Error('error in saving a user')
-                        return done(null, newUser)
-                    })
-                }
-            })
+    }, function(token, refreshToken, profile, done) {
+            process.nextTick(function() {
+                User.findOne({'github.id': profile.id}, function(err, user) {
+                    if (err) return done(err)
+                    if (user) return done(null, user)
+                    else {
+                        var newUser = new User()
+                        
+                        newUser.github.id = profile.id
+                        newUser.github.username = profile.username
+                        newUser.github.displayName = profile.displayName
+                        newUser.github.publicRepos = profile._json.public_repos
+                        newUser.nbrClicks.clicks = 0
+                        
+                        newUser.save(function(err) {
+                            if (err) throw new Error('error in saving a user')
+                            return done(null, newUser)
+                        })
+                    }
+                })
         })
-    })
+    }))
 }
--- server.js
+++ server.js
@@ -3,15 +3,29 @@
 var express = require('express'),
     routes = require('./app/routes/index.js'),
     mongoose = require('mongoose'),
+    passport = require('passport'),
+    session = require('express-session'),
     app = express()
     
-mongoose.connect('mongodb://' + process.env.IP + ':27017/clementinedb')
+require('dotenv').load()
+require('./app/config/passport')(passport)
+    
+mongoose.connect(process.env.MONGO_URI)
     
 app.use('/public', express.static(process.cwd() + '/public'))
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
+
+app.use(session({
+    secret: 'secretClementine',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
     
-routes(app)
+routes(app, passport)
 
 var server = app.listen(process.env.PORT || 8080, function() {
-    console.log('Listening on port ' + server.address().port)
+    console.log('Listening on port ' + server.address().port + '...')
 })
